0cf629f742ae96cb881717da5cd6309b5b819ce7
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2024-04-26 12:38:49 +0200

    Added AI integration

--- src/script.js
+++ src/script.js
@@ -1,13 +1,29 @@
-function generateSuggestion(event) {
-  event.preventDefault();
-
+function showSuggestions(response) {
+  console.log("suggestions generated");
   new Typewriter("#response", {
-    strings:
-      "Among the most popular songs of the Beatles are: All you need is love, Yellow Submarine and Let it be",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateSuggestion(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instructions");
+  console.log(instructionInput.value);
+  let apiKey = "b0c48dt2da1edfa05b13oc7376330d93";
+  let context =
+    "You are an AI who knwos a lot about music and who likes to help people to find new music to listen to, always keeping in mind to follow the user instructions. Please give some additional information to each suggestion.";
+  let prompt = `User instructions: Please suggest songs or musicians similar to ${instructionInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating suggestions");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(showSuggestions);
 }
 
 let formElement = document.querySelector("#input-form");
