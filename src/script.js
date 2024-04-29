07905412cabe18306d3d087cb0ca897ec879f9ed
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2024-04-29 11:02:09 +0200

    Updated AI context and CSS

--- src/script.js
+++ src/script.js
@@ -14,13 +14,13 @@ function generateSuggestion(event) {
   console.log(instructionInput.value);
   let apiKey = "b0c48dt2da1edfa05b13oc7376330d93";
   let context =
-    "You are an AI who knows a lot about music and who likes to help people to find new and unusual music to listen to, always keeping in mind to follow the user instructions. Always start with the <h1> 'Music Tips for fans of instructionInput.value' Craft a list of 5 music tips in basic HTML format and give additional information and song tips to each tip. Use bullet points and no stars.";
+    "You are an AI who knows a lot about music and who likes to help people to find new and unusual music to listen to, always keeping in mind to follow the user instructions. Please follow these instructions: Always start with the <h1> 'Music Tips for Fans of instructionInput.value'. Craft a list with bullet points of 5 music tips and give additional information and song tips. Use a <br/> element AFTER every tip. Use quotations marks for names.";
   let prompt = `User instructions: Please suggest songs or musicians similar to ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let suggestionElement = document.querySelector("#response");
   suggestionElement.classList.remove("hidden");
-  suggestionElement.innerHTML = `<div class="generating">⏳ Findining new music similar to ${instructionInput.value}</div>`;
+  suggestionElement.innerHTML = `<div class="generating">⏳ Searching for music similar to ${instructionInput.value}</div>`;
 
   axios.get(apiUrl).then(showSuggestions);
 }
